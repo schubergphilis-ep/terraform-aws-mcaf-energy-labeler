b2589d8b0ec188d1022d8eb242ca6c07e44932d3
Keith Segovia <ksegovia@schubergphilis.com>
2025-11-07 16:47:48 +0100

    scope down resources for kms_key_policy policy document

--- kms.tf
+++ kms.tf
@@ -22,7 +22,7 @@ data "aws_iam_policy_document" "kms_key_policy" {
   statement {
     sid       = "Read/List permissions for all IAM users"
     effect    = "Allow"
-    resources = ["*"]
+    resources = ["arn:aws:kms:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:key/*"]
 
     actions = [
       "kms:Describe*",
